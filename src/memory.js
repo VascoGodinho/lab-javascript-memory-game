23998d346b035a54613826e3849c40c95eee5a5b
Vasco Godinho <vascogodinho88@live.com.pt>
2023-11-29 17:23:47 +0000

    1st commit

--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,57 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0; 
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards)
+    {
+      return undefined
+    }
+     
+    for (let index = this.cards.length - 1 ; index > 0 ; index -= 1)
+    {
+       const shuffled = Math.floor(Math.random() * (index + 1))
+       const temp = this.cards[index]
+       this.cards[index] = this.cards[shuffled];
+       this.cards[shuffled] = temp;
+       
+    }
+     return this.cards
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    
+    this.pairsClicked += 1;
+    if (card1 === card2)
+    {
+      
+      this.pairsGuessed += 1;
+
+      return true
+    }
+    else 
+    {
+      return false
+    }
+
+    
   }
 
   checkIfFinished() {
-    // ... write your code here
-  }
+    if(this.pairsGuessed === 0 || this.pairsGuessed !== this.cards.length /2)
+    {
+      return false
+    }
+   
+    if(this.pairsGuessed === this.cards.length / 2)
+   {
+    return true
+   }
+  
+}
 }
